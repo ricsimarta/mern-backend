52932211ed53712ea85a4b6975817a62a6294742
ricsimarta <ricsimarta@gmail.com>
2024-11-29 18:41:00 +0100

    added cors

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+
 const booksRoutes = require('./routes/booksRoutes.js');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
+
 app.use('/api/books', booksRoutes);
 
 app.listen(5000, () => console.log("server is running"));
